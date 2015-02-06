85831336ef064b3e317a2b314bba67fef6ae9727
kisang <user@localhost.localdomain>
2015-02-06 20:05:28 +0900

    button event

--- Do-I-Source/app/src/main/java/com/example/user/do_i/MainActivity.java
+++ Do-I-Source/app/src/main/java/com/example/user/do_i/MainActivity.java
@@ -4,14 +4,28 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.btn1);
+        btn.setText("환승 되?");
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "환승 아직 안되 ㅠㅠ", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 
 
