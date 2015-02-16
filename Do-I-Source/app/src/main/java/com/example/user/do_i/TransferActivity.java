21926fc5086452f69772ba68f6440f2854969da5
kisang <user@localhost.localdomain>
2015-02-17 00:09:33 +0900

    Create time algorism

--- Do-I-Source/app/src/main/java/com/example/user/do_i/TransferActivity.java
+++ Do-I-Source/app/src/main/java/com/example/user/do_i/TransferActivity.java
@@ -10,22 +10,27 @@ import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 /**
  * Created by user on 15. 2. 7.
  */
 
 public class TransferActivity extends Activity {
 
-    Button btn2;
     Chronometer ct;
-    int number = 3;
     TextView allowTransfer;
+    int number = 3;
+    int allowTime;
+    final int longTrasferTime = 10;
+    final int shortTransferTime = 5;
+
 
     //If transfer allow text hander
     Handler timeHandler = new Handler(){
         public void handleMessage(Message msg){
             long time = (SystemClock.elapsedRealtime() - ct.getBase()) / 1000;
-            if(time<10) allowTransfer.setText("환승 가능!");
+            if(time<allowTime) allowTransfer.setText("환승 가능!");
             else {
                 allowTransfer.setText("환승 불가!");
                 timeHandler.removeMessages(0);
@@ -39,15 +44,6 @@ public class TransferActivity extends Activity {
         super.onCreate(savedINstanceState);
         setContentView(R.layout.transfor);
 
-/*
-        final Calendar currentTime = Calendar.getInstance();
-        int Hour = currentTime.get(Calendar.HOUR_OF_DAY);
-        int Minute = currentTime.get(Calendar.MINUTE);
-        int Second = currentTime.get(Calendar.SECOND);
-        int AmPm = currentTime.get(Calendar.AM_PM);
-
-        text2.setText(Hour +"시 " + Minute + "분 " + Second + "초");
-*/
         final TextView text1 = (TextView)findViewById(R.id.TextView1);
         allowTransfer = (TextView)findViewById(R.id.TextView2);
         ct = (Chronometer) findViewById(R.id.chronmeter1);
@@ -55,22 +51,22 @@ public class TransferActivity extends Activity {
         text1.setText("환승 가능 횟수 : -");
         allowTransfer.setText("환승 가능 여부 : - ");
 
-        //time = (SystemClock.elapsedRealtime() - ct.getBase()) / 1000;
-        //text2.setText("" + time);
-
-        //Allow transfer confirm Handler
-
-
-
-
+        //Click do transfer Button
         findViewById(R.id.button0).setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
+                        Calendar currentTime = Calendar.getInstance();
+                        int Hour = currentTime.get(Calendar.HOUR_OF_DAY); //receive current Hour
+                        if(Hour>=7 && Hour<=21) allowTime = shortTransferTime;
+                        else allowTime = longTrasferTime;
+
+                        timeHandler.removeMessages(0);  //remove timer handler
                         ct.setBase(SystemClock.elapsedRealtime());
                         ct.start();
                         if (number == 0) {
                             ct.stop();
                             text1.setText("환승 가능 횟수 :  환승 불가");
+                            allowTransfer.setText("환승 불가!");
                         } else {
                             text1.setText("환승 가능 횟수 : " + number);
                             number--;
@@ -80,10 +76,12 @@ public class TransferActivity extends Activity {
                     }
                 });
 
-
-        findViewById(R.id.button1).setOnClickListener(      //Init(초기화) button event
+        //Init(초기화) button event
+        findViewById(R.id.button1).setOnClickListener(
                 new Button.OnClickListener(){
                     public void onClick(View v){
+                        ct.stop();
+                        allowTransfer.setText("환승 가능 여부 : - ");
                         timeHandler.removeMessages(0);
                         number = 3;
                         text1.setText("환승 가능 횟수 :  -");
@@ -91,13 +89,13 @@ public class TransferActivity extends Activity {
                     }
                 }
         );
+        //back activity
+        findViewById(R.id.button2).setOnClickListener(
+                new View.OnClickListener(){
 
-        btn2 = (Button) findViewById(R.id.button2);     //back activity
-        btn2.setOnClickListener(new View.OnClickListener(){
-
-            public void onClick(View v){
-                finish();
-            }
-        });
-    }
+                    public void onClick(View v){
+                        finish();
+                    }
+                });
+        }
 }
